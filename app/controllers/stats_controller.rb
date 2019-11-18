0fd22f04c475fa41011926e8e3a69d2ae8a80fdc
Adam Losher <adamlosher@gmail.com>
2019-11-18 12:24:22 -0600

    finished roll def

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,17 +2,22 @@ class StatsController < ApplicationController
   
   def new
     Stats.level = 1
-
+    
   end
   
-  def roll(sides, number)
+  def stats_roll(sides, number)
     roll_array = []
     number.times do
       roll_value = rand(sides) + 1
       roll_array << roll_value
     end
-    roll_array.sort.reverse
-    
+    roll_array = roll_array.sort.reverse
+    roll_array.pop
+    roll_array.pop
+    total = roll_array.sum
+    if total < 10
+      total = 10
+    total
   end
 
 end
